6484a766c4f89f1ac02784ea0c118b77d0090119
Raimundo Lameira <raimundonorberto@gmail.com>
2020-01-16 09:18:53 -0300

    Commit inicial. Backend.

---
+++ backend/src/models/util/PointSchema.js
@@ -0,0 +1,15 @@
+const mongooge = require('mongoose');
+
+const PointSchema = new mongooge.Schema({
+    type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+    },
+    coordinates: {
+        type: [Number],
+        required: true,
+    },
+});
+
+module.exports = PointSchema;
