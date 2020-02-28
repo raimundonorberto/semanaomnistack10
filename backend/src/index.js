33daf26c835f0cfb85d1b9a431fde5c6ac3097ee
Raimundo Lameira <raimundonorberto@gmail.com>
2020-02-27 22:32:04 -0300

    Instalação e configuração das libs dotenv e cors.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://<username>:<password>@omnistack-hfo4o.mongodb.net/week10?retryWrites=true&w=majority', {
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
